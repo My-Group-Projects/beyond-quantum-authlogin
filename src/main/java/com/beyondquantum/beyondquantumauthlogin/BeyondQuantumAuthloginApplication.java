1afb39ccc1ed5ce50af05ec0745d599bce82b4ca
abhaybhandari <69076519+AbhayBhandari@users.noreply.github.com>
2023-12-31 20:16:50 +0530

    Repo setup initial commit

---
+++ src/main/java/com/beyondquantum/beyondquantumauthlogin/BeyondQuantumAuthloginApplication.java
@@ -0,0 +1,13 @@
+package com.beyondquantum.beyondquantumauthlogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeyondQuantumAuthloginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeyondQuantumAuthloginApplication.class, args);
+	}
+
+}
